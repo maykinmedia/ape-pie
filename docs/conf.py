a4cc5ab06069a9600a10ed8416fa148c61730ac6
Chris Wesseling <chris@maykinmedia.nl>
2024-06-18 12:19:35 +0200

    :bookmark: Bump to 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "ape-pie"
 copyright = "2023, Maykin Media"
 author = "Maykin Media"
-release = "0.1.0"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
